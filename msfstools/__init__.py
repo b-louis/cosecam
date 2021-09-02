6a3a22ca888c392329f8ad7fc285c49ac5c7ac3f
b-louis <lb.gnipegou@gmail.com>
2021-09-02 15:43:50 +0200

    cleaner code and some small fixes

--- msfstools/__init__.py
+++ msfstools/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ["msfs_rec", "msfs_dec"]
+import os
+if os.name == 'nt':
+    __all__ = ["msfs_rec", "msfs_dec"]
+else:
+    __all__ = ["msfs_rec"]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,3 @@
-# from .homography_new import *
-# from .morph import *
 import os
-common = ["homography_new", "homography","georef","msfs_dec"]
-if os.name == 'nt':
-    __all__ = common+["msfs_rec"]
-else:
-    __all__ = common
+__all__ = ["homography","features"]
+
